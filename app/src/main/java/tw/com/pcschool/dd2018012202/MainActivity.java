b60d6b1d0681d0ceb866da9f4967410292cbd73b
Teacher <shaojung@gmail.com>
2018-01-22 13:53:06 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/dd2018012202/MainActivity.java
@@ -0,0 +1,16 @@
+package tw.com.pcschool.dd2018012202;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyHelper helper = new MyHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+    }
+}
